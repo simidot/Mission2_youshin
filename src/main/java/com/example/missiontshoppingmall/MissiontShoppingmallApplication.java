54fbf1b78af2b03726a85ffb58618b7a44e5db83
sim <sgi032151@naver.com>
2024-02-22 20:45:29 +0900

    init

---
+++ src/main/java/com/example/missiontshoppingmall/MissiontShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.missiontshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissiontShoppingmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissiontShoppingmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/missiontshoppingmall/MissiontShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.missiontshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissiontShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
