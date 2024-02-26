30a0cbf6a348300fd1cdca632891cd6748711693
sim <sgi032151@naver.com>
2024-02-26 21:08:37 +0900

    Feat: 쇼핑몰 개설신청 기능 구현
    1. 관리자가 사업자신청 승인시 바로 쇼핑몰 객체 추가
    2. 쇼핑몰 필수정보 작성 후 개설신청 가능

---
+++ src/main/java/com/example/missiontshoppingmall/shoppingMall/ShoppingMallController.java
@@ -0,0 +1,25 @@
+package com.example.missiontshoppingmall.shoppingMall;
+
+import com.example.missiontshoppingmall.shoppingMall.dto.MallOpenRequest;
+import com.example.missiontshoppingmall.shoppingMall.dto.MallOpenResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/shopping-mall")
+public class ShoppingMallController {
+    private final ShoppingMallService mallService;
+
+    // 쇼핑몰 개설
+    // 사업자 사용자로 전환되며 준비중 상태의 쇼핑몰이 추가된 상태
+    @PostMapping("/{mallId}")
+    public MallOpenResponse openRequest(
+            @PathVariable("mallId") Long id,
+            @RequestBody MallOpenRequest requestDto
+    ) {
+        return mallService.createOpenRequest(requestDto);
+    }
+}
